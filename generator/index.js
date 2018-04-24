a17a22c9f12e0884ce58931d4b8f8a4e14de2c79
dgpgdev <dgpgdev@gmail.com>
2018-04-24 10:03:30 +0200

    enabled custom config prompt first

--- generator/index.js
+++ generator/index.js
@@ -1,14 +1,14 @@
 
 module.exports = (api, options, rootOptions) => {
-   const fs = require('fs')
-   const util = require('util')
-
+  const fs = require('fs')
+  const util = require('util')
+  const rootDoc = options.rootDoc || 'docs'
    
   // modify package.json fields
   api.extendPackage({
     scripts: {
-      'docs:dev': `vuepress dev ${options.rootDoc}` ,
-      'docs:build': `vuepress build ${options.rootDoc}`
+      'docs:dev': `vuepress dev ${rootDoc}` ,
+      'docs:build': `vuepress build ${rootDoc}`
     },
     devDependencies: {
       vuepress: '^0.7.0'
@@ -30,14 +30,14 @@ module.exports = (api, options, rootOptions) => {
     const path = require('path') 
     const shell = require('shelljs')
     //create .vuepress folder
-    shell.mkdir('-p', path.resolve(options.rootDoc+'/.vuepress'))
+    shell.mkdir('-p', path.resolve(rootDoc+'/.vuepress'))
     //conpy files inside .vuepress
     const cpFiles = ['README.md', 'page1.md', 'page2.md', 'page3.md']
     for(const f of cpFiles){
-      shell.cp(path.resolve(__dirname+'/templates/'+f), path.resolve(options.rootDoc+'/'+f))
+      shell.cp(path.resolve(__dirname+'/templates/'+f), path.resolve(rootDoc+'/'+f))
     }
     //write config js file
-    fs.writeFile(path.resolve(options.rootDoc+'/.vuepress/config.js'),conf, error=> {
+    fs.writeFile(path.resolve(rootDoc+'/.vuepress/config.js'),conf, error=> {
       if(error) throw error
     })    
   })
--- prompts.js
+++ prompts.js
@@ -1,19 +1,28 @@
 module.exports = [
   {
+    type: 'confirm',
+    name: 'enabledConfig',
+    message: '📖 custom configuration file ?',
+    default: false,
+  },
+  {
     type: 'input',
     name: 'rootDoc',
     message: 'input docs directory',
     default: 'docs',
+    when: answers => answers.enabledConfig
   },
   {
     type: 'input',
     name: 'title',
     message: 'Title for the site (default project name)',
+    when: answers => answers.enabledConfig
   },
   {
     type: 'input',
     name: 'output',
     message: 'Output directory',
-    default: 'vuepress'
+    default: 'vuepress',
+    when: answers => answers.enabledConfig
   }
 ]
